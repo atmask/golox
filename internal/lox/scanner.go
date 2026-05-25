b485d3e7721f5d638af43fcadf786fa74dd3c7a5
Ben Mask <ben.mask@fellow.co>
2026-05-25 09:12:00 -0400

    [4.5] Scanning token types and lexemes

--- internal/lox/scanner.go
+++ internal/lox/scanner.go
@@ -29,6 +29,42 @@ func (s *Scanner) isAtEnd() bool {
 	return s.current >= len(s.source)
 }
 
+func (s *Scanner) advance() byte {
+	ch := s.source[s.current]
+	s.current++
+	return ch
+
+}
+
+func (s *Scanner) addToken(t TokenType, literal any) {
+	text := s.source[s.start:s.current]
+
+	s.tokens = append(s.tokens, Token{t, text, literal, s.line})
+}
+
 func (s *Scanner) scanToken() {
-	s.tokens = append(s.tokens, Token{})
+	// s.tokens = append(s.tokens, Token{})
+	ch := s.advance()
+	switch ch {
+	case '(':
+		s.addToken(LEFT_PAREN, nil)
+	case ')':
+		s.addToken(RIGHT_PAREN, nil)
+	case '{':
+		s.addToken(LEFT_BRACE, nil)
+	case '}':
+		s.addToken(RIGHT_BRACE, nil)
+	case ',':
+		s.addToken(COMMA, nil)
+	case '.':
+		s.addToken(DOT, nil)
+	case '-':
+		s.addToken(MINUS, nil)
+	case '+':
+		s.addToken(PLUS, nil)
+	case ';':
+		s.addToken(SEMICOLON, nil)
+	case '*':
+		s.addToken(STAR, nil)
+	}
 }
