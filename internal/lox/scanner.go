cec9db808da799995d0a401978f1219f5672ea05
Ben Mask <ben.mask@fellow.co>
2026-05-25 11:01:23 -0400

    [4.5.2] Operator support

--- internal/lox/scanner.go
+++ internal/lox/scanner.go
@@ -30,6 +30,8 @@ func (s *Scanner) isAtEnd() bool {
 	return s.current >= len(s.source)
 }
 
+// Reads the current byte and then advances
+// Returns the read byte
 func (s *Scanner) advance() byte {
 	ch := s.source[s.current]
 	s.current++
@@ -37,6 +39,22 @@ func (s *Scanner) advance() byte {
 
 }
 
+// Conditional form of advance()
+// Advances iff the next byte == expected
+func (s *Scanner) match(expected byte) bool {
+	if s.isAtEnd() {
+		return false
+	}
+	if s.source[s.current] != expected {
+		return false
+	}
+
+	s.current++
+	return true
+
+}
+
+// Given a TokenType and literal, append a Token to the instance slice
 func (s *Scanner) addToken(t TokenType, literal any) {
 	text := s.source[s.start:s.current]
 
@@ -67,6 +85,30 @@ func (s *Scanner) scanToken() {
 		s.addToken(SEMICOLON, nil)
 	case '*':
 		s.addToken(STAR, nil)
+	case '!':
+		if s.match('=') {
+			s.addToken(BANG_EQUAL, nil)
+		} else {
+			s.addToken(BANG, nil)
+		}
+	case '=':
+		if s.match('=') {
+			s.addToken(EQUAL_EQUAL, nil)
+		} else {
+			s.addToken(EQUAL, nil)
+		}
+	case '<':
+		if s.match('=') {
+			s.addToken(LESS_EQUAL, nil)
+		} else {
+			s.addToken(LESS, nil)
+		}
+	case '>':
+		if s.match('=') {
+			s.addToken(GREATER_EQUAL, nil)
+		} else {
+			s.addToken(GREATER, nil)
+		}
 	default:
 		s.errors = append(s.errors, ScanError{s.line, "Unexpected character: " + string(ch)})
 	}
